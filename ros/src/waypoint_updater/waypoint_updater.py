d1f95f3dc9c18eecb0a7652cf81ebb35918f0a12
MohammedAbdou285 <Mohammed.Abdou@valeo.com>
2018-07-28 06:23:25 +0200

    Modify way_updater to be 20 points to be updated and depend on traffic_light as a ground truth

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -26,7 +26,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
 MAX_DECEL = 0.5
 
 
@@ -56,7 +56,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(20)
         while not rospy.is_shutdown():
             #if self.pose and self.base_waypoints:
             if self.pose and self.base_lane:
